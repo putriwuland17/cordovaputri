7a1dcc884d93cae23cd18330f4772447e1b64411
putriwuland17 <72685511+putriwuland17@users.noreply.github.com>
2021-03-24 21:38:44 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod? api_key = K8inilV4HsKRquNzc9mKFI6CcMCfhbzagoLldtYB";
         
         $("#loadImage").click(function(){
             $.ajax({
